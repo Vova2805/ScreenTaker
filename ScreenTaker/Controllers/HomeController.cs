a3d0a915280583a64c757e063d3ca1fac292dabe
Roman <Roman@DESKTOP-7NC1F6V>
2016-05-20 16:34:17 +0300

    packages updated added library view edited master layout

--- ScreenTaker/Controllers/HomeController.cs
+++ ScreenTaker/Controllers/HomeController.cs
@@ -26,5 +26,50 @@ namespace ScreenTaker.Controllers
 
             return View();
         }
+
+        public ActionResult Library()
+        {
+            ViewBag.Message = "Library page";
+
+            ViewBag.Folders = folders;
+
+            return View();
+        }
+
+        // todo: database context
+        public struct Folder
+        {
+            public int BookId { get; set; }
+            public string Title { get; set; }
+            public bool IsPrivate { get; set; }
+
+            public Folder(int bookId, string title, bool isPrivate)
+            {
+                BookId = bookId;
+                Title = title;
+                IsPrivate = isPrivate;
+            }
+        }
+        // represent db context
+        List<Folder> folders = new List<Folder>()
+            {
+                new Folder(0, "My Photo", false),
+                new Folder(1, "Fishing", true),
+                new Folder(2, "Job", false),
+                new Folder(3, "Friends", false),
+                new Folder(4, "Kids", true),
+                new Folder(5, "Summer-2015", false),
+                new Folder(6, "Africa", true),
+                new Folder(7, "Encapsulation", true)
+            };
+
+        [HttpPost]
+        public ActionResult ChangeFoldersAttr(Folder folder)
+        {
+            // modify dbc
+            folders[folder.BookId] = folder;
+
+            return RedirectToAction("Library");
+        }
     }
 }
