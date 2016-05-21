f498e423bc6e60649da730863165a8236f351ed9
alex bachkay <abachkay@gmail.com>
2016-05-21 19:37:56 +0300

    upload screenshot feature added

--- ScreenTaker/Controllers/HomeController.cs
+++ ScreenTaker/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,18 @@ namespace ScreenTaker.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/img/"), fileName);
+                file.SaveAs(path);
+            }
             return View();
         }
 
