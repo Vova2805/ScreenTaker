33e3f4714aacc8be202afc8468853166365642d8
Volodymyr Dudas <vovadudas@gmail.com>
2016-05-22 21:58:36 +0300

    Image View. Header doesn't work yet

--- ScreenTaker/Controllers/HomeController.cs
+++ ScreenTaker/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace ScreenTaker.Controllers
 {
     public class HomeController : Controller
     {
+        ScreenTakerDBEntities _entities = new ScreenTakerDBEntities();
         public ActionResult Index()
         {
             return View();
@@ -33,61 +34,19 @@ namespace ScreenTaker.Controllers
         {
             ViewBag.Message = "Library page";
 
-            ViewBag.Folders = folders;
+            ViewBag.Folders = _entities.folder.ToList(); ;
 
             return View();
         }
 
-        // todo: database context
-        public struct Folder
-        {
-            public int BookId { get; set; }
-            public string Title { get; set; }
-            public bool IsPrivate { get; set; }
-
-            public Folder(int bookId, string title, bool isPrivate)
-            {
-                BookId = bookId;
-                Title = title;
-                IsPrivate = isPrivate;
-            }
-        }
-        // represent db context
-        List<Folder> folders = new List<Folder>()
-            {
-                new Folder(0, "My Photo", false),
-                new Folder(1, "Fishing", true),
-                new Folder(2, "Job", false),
-                new Folder(3, "Friends", false),
-                new Folder(4, "Kids", true),
-                new Folder(5, "Summer-2015", false),
-                new Folder(6, "Africa", true),
-                new Folder(7, "Encapsulation", true)
-            };
-
         [HttpGet]
-        public ActionResult ChangeFoldersAttr(Folder folder)
+        public ActionResult ChangeFoldersAttr(folder folder)
         {
-            // modify dbc
-            folders[folder.BookId] = folder;
+            //folders[folder.BookId] = folder;
 
             return RedirectToAction("Library");
         }
         #endregion
-
-        public class MyImage
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Source { get; set; }
-
-            public MyImage(int id, string name, string source)
-            {
-                Id = id;
-                Name = name;
-                Source = source;
-            }
-        }
 
         public static List<string> GetImageList()
         {
@@ -96,11 +55,9 @@ namespace ScreenTaker.Controllers
             return l;
         }
 
-        private List<string> images = GetImageList();
-
         public ActionResult Images()
         {
-            ViewBag.ImagePath = images;
+            ViewBag.ImagePath = _entities.image.ToList().Select(im=>GetBaseUrl()+"img/"+im.name+".png").ToList();
 
             return View();
         }
