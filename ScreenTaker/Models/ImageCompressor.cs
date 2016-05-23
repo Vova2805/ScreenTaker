ee2304711ea787cc235ded4127511715cc16565d
Volodymyr Dudas <vovadudas@gmail.com>
2016-05-23 13:35:21 +0300

    Image Compressor added

---
+++ ScreenTaker/Models/ImageCompressor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+
+namespace ScreenTaker.Models
+{
+    public class ImageCompressor
+    {
+        public Bitmap Compress(Bitmap originalBM, Size compressedImageSize)
+        {                     
+            var size = new Size();
+            if (originalBM.Size.Height < originalBM.Size.Width)
+                size = new Size(Convert.ToInt32(compressedImageSize.Height * 1.0 / originalBM.Size.Height * 1.0 * originalBM.Size.Width * 1.0), compressedImageSize.Height);
+            else
+                size = new Size(compressedImageSize.Width, Convert.ToInt32(compressedImageSize.Width * 1.0 / originalBM.Size.Width * 1.0 * originalBM.Size.Height * 1.0));
+
+            if (size.Width != 0 && size.Height != 0)
+            {
+                Bitmap resizedBM = new Bitmap(originalBM, size);
+                if (originalBM.Size.Height < originalBM.Size.Width)
+                    resizedBM = CropImage(resizedBM, new Rectangle(new Point(Convert.ToInt32((size.Width - compressedImageSize.Width) / 2.0), 0), new Size(compressedImageSize.Width, compressedImageSize.Height)));
+                else
+                    resizedBM = CropImage(resizedBM, new Rectangle(new Point(0, Convert.ToInt32((size.Height - compressedImageSize.Height) / 2.0)), new Size(compressedImageSize.Width, compressedImageSize.Height)));
+                return resizedBM;
+            }
+            return null;
+        }
+
+        private Bitmap CropImage(Bitmap source, Rectangle section)
+        {
+            Bitmap bmp = new Bitmap(section.Width, section.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+            return bmp;
+        }
+    }
+}
