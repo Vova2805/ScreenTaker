654ed7429ab20370e9b790fcfabdcd5bfda9ace6
Dudas <Dudas@VOVA-PC>
2016-05-25 13:14:34 +0300

    Create view and controller for editing user groups

---
+++ ScreenTaker/Controllers/EditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScreenTaker.Controllers
+{
+    public class EditController : Controller
+    {
+        // GET: Edit
+        public ActionResult UserGroups()
+        {
+            return View();
+        }
+    }
+}
