f6740d4de4a6c6e387623f04afd3536bc554df4a
alex bachkay <abachkay@gmail.com>
2016-05-28 16:06:16 +0300

    added group create, delete and select features

--- ScreenTaker/Controllers/EditController.cs
+++ ScreenTaker/Controllers/EditController.cs
@@ -14,25 +14,48 @@ namespace ScreenTaker.Controllers
 {
     public class EditController : Controller
     {
+        private ScreenTakerEntities _entities = new ScreenTakerEntities();
         public ActionResult Index(string lang = "en")
         {
             return View("UserGroups");
         }
 
-        public ActionResult UserGroups(string lang = "en")
+        public ActionResult UserGroups(string lang = "en",int selectedId=0)
         {
+            using (var transaction = _entities.Database.BeginTransaction())
+            {
+                ViewBag.selectedId = selectedId;
+                try
+                {
+                    ViewBag.Groups = _entities.PersonGroups.Select(s => s).ToList();              
+                    ViewBag.GroupMemberCounts= _entities.PersonGroups.Select(s => s.GroupMembers.Count).ToList();
+
+                    var emails = from p in _entities.People
+                                 join m in _entities.GroupMembers
+                                 on p.Id equals m.PersonId
+                                 where m.GroupId == selectedId
+                                 select new { ID = m.GroupId, Email = p.Email };
+                    if (emails.Any())
+                        ViewBag.Emails = emails.Select(s => s.Email).ToList();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
             return View();
         }
 
-        public void ChangeLocalization(string request,string lang = "en")
+        public void ChangeLocalization(string request, string lang = "en")
         {
-            if(request.Contains("?"))
+            if (request.Contains("?"))
             {
-                if(request.Contains("lang"))
+                if (request.Contains("lang"))
                 {
                     int index = request.IndexOf("lang=");
                     string sub = request.Substring(index, 7);
-                    request = request.Replace(sub,"lang="+lang);
+                    request = request.Replace(sub, "lang=" + lang);
                 }
                 else
                 {
@@ -43,7 +66,51 @@ namespace ScreenTaker.Controllers
             {
                 request += "?lang=" + lang;
             }
-             Response.Redirect(request);
+            Response.Redirect(request);
         }
+
+        public ActionResult CreateGroup(string name)
+        {
+
+            using (var transaction = _entities.Database.BeginTransaction())
+            {
+                try
+                {
+                    var group = new PersonGroup();
+                    group.Name = name;
+                    group.PersonId = null;
+                    _entities.PersonGroups.Add(group);
+                    _entities.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
+            return RedirectToAction("UserGroups");
+        }
+
+
+        public ActionResult RemoveGroup(int selectedId = 0)
+        {
+            using (var transaction = _entities.Database.BeginTransaction())
+            {
+                try
+                {
+                    var groupId = selectedId;
+                    var group = _entities.PersonGroups.Where(w => w.Id == groupId).FirstOrDefault();
+                    _entities.PersonGroups.Remove(group);
+                    _entities.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
+            return RedirectToAction("UserGroups");
+        }
+
     }
 }
