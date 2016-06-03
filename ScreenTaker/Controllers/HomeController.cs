d0d67d04185ad7db1dc3210d316c26d4d9b8af80
Dudas <Dudas@VOVA-PC>
2016-06-03 19:57:11 +0200

    Bug fixing

--- ScreenTaker/Controllers/HomeController.cs
+++ ScreenTaker/Controllers/HomeController.cs
@@ -190,8 +190,8 @@ namespace ScreenTaker.Controllers
         {
             ViewBag.Localize = locale;
 
-           // var sharedСode = Path.GetFileNameWithoutExtension(path);
-            var result = _entities.Folders.FirstOrDefault(w => w.Id  == folderId);
+            // var sharedСode = Path.GetFileNameWithoutExtension(path);
+            var result = _entities.Folders.FirstOrDefault(w => w.Id == folderId);
 
             if (result.IsPublic)
 
@@ -291,92 +291,6 @@ namespace ScreenTaker.Controllers
                ViewBag.Folders.Count > 0 ? _entities.Folders.Where(f => f.OwnerId == user.Id).ToList().First().Id : -1
                ) : folder.Id;
             return View();
-        }
-
-
-        public ActionResult MakeFolderPublicOrPrivate(int folderId, string lang = "en")
-        {
-            ViewBag.Localize = locale;
-
-           // var sharedСode = Path.GetFileNameWithoutExtension(path);
-            var result = _entities.Folders.FirstOrDefault(w => w.Id  == folderId);
-
-            if (result.IsPublic)
-
-                result.IsPublic = false;
-
-            else
-                result.IsPublic = true;
-
-            _entities.SaveChanges();
-
-
-            return RedirectToAction("SingleImage", new { lang = locale });
-        }
-
-
-        public ActionResult FolderSwitchAccessOnOrOff(int groupId, int folderId)
-        {
-                var result = _entities.GroupShares.Where(w => w.GroupId == groupId && w.FolderId == folderId).Select(s => s.Id).FirstOrDefault();
-            if (result != 0)
-                _entities.GroupShares.Remove(_entities.GroupShares.FirstOrDefault(w => w.Id == result));
-            else
-            {
-
-                GroupShare us = new GroupShare
-                {
-                    GroupId = groupId,
-                    FolderId = folderId
-                };
-                _entities.GroupShares.Add(us);
-            }
-
-
-            _entities.SaveChanges();
-            return RedirectToAction("SingleImage");
-        }
-
-
-        public ActionResult FoldersAddMail(string userMail, int folderId)
-        {
-        
-            using (var transaction = _entities.Database.BeginTransaction())
-            {
-                try
-                {
-
-                    var personID =
-                              _entities.People.Where(w => w.Email == userMail).Select(s => s.Id).FirstOrDefault();
-
-                    if (personID != 0)
-                    {
-
-                        UserShare us = new UserShare
-                        {
-                            PersonId = personID,
-                            FolderId = folderId
-                        };
-                        _entities.UserShares.Add(us);
-                    }
-                    else
-                    {
-                        UserShare us = new UserShare
-                        {
-                            FolderId = folderId,
-                            Email = userMail
-                        };
-                        _entities.UserShares.Add(us);
-                    }
-                    _entities.SaveChanges();
-                    transaction.Commit();
-                }
-                catch (Exception e)
-                {
-                    transaction.Rollback();
-                }
-
-            }
-            return RedirectToAction("SingleImage");
         }
 
         [HttpGet]
