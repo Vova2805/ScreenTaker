8dd13eae8f32e820ba929dc51696011b8b8c46d9
dyvak <ihor.dyvak.pi2013@lpnu.ua>
2016-06-04 03:43:42 +0300

    Edit security check queries.

--- ScreenTaker/Models/SecurityHelper.cs
+++ ScreenTaker/Models/SecurityHelper.cs
@@ -13,6 +13,39 @@ namespace ScreenTaker.Models
             return folderPath + "/" + imageCode + ".png";
         }
 
+        public static List<Image> GetAccessibleImages(ApplicationUser user, Folder folder, ScreenTakerEntities context)
+        {
+            if (IsFolderAccessible(user, folder.Person, folder, context))
+            {
+                return folder.Images.ToList();
+            }
+            if (user == null)
+            {
+                return null;
+            }
+            return ((
+                    from image in context.Images
+                    where image.IsPublic 
+                    select image)
+                .Union(
+                    from image in context.Images
+                    join us in context.UserShares
+                        on new { iid = (int?)image.Id, fid = image.FolderId } equals new { iid = us.ImageId, fid = folder.Id }
+                    select image)
+                .Union(
+                    from im in context.Images
+                    where im.FolderId == folder.Id
+                    join gs in context.GroupShares
+                        on im.Id equals gs.ImageId
+                    join pg in context.PersonGroups
+                        on gs.GroupId equals pg.Id
+                    join gm in context.GroupMembers
+                        on pg.Id equals gm.GroupId
+                    where gm.PersonId == user.Id
+                    select im)
+            ).ToList();
+        } 
+
         public static bool IsImageAccessible(ApplicationUser user, Person owner, Image image, ScreenTakerEntities context)
         {
             if (image.IsPublic)
@@ -20,19 +53,33 @@ namespace ScreenTaker.Models
             if (user == null)
                 return false;
             return image.Folder.OwnerId == user.Id
-                   || context.UserShares.Any(us => us.PersonId == user.Id && us.ImageId == image.Id)
+                   || context.UserShares.Any(
+                       us => us.PersonId == user.Id && (us.ImageId == image.Id || us.FolderId == image.FolderId))
                    || (from gm in context.GroupMembers
                        join pg in context.PersonGroups
                            on new {pid = gm.PersonId, gid = gm.GroupId} equals new {pid = user.Id, gid = pg.Id}
                        join gs in context.GroupShares
                            on pg.Id equals gs.GroupId
-                       where gs.ImageId == image.Id
-                       select gm.PersonId).Any();
+                       where gs.ImageId == image.Id || gs.FolderId == image.FolderId
+                       select gm.PersonId).Any()
+                   || context.UserShares.Any(us => us.PersonId == user.Id && us.ImageId == image.Id);
         }
 
         public static bool IsFolderAccessible(ApplicationUser user, Person owner, Folder folder, ScreenTakerEntities context)
         {
-            return folder.IsPublic || folder.OwnerId == user?.Id;
+            if (folder.IsPublic)
+                return true;
+            if (user == null)
+                return false;
+            return folder.OwnerId == user.Id
+                   || context.UserShares.Any(us => us.PersonId == user.Id && us.FolderId == folder.Id)
+                   || (from gm in context.GroupMembers
+                       join pg in context.PersonGroups
+                           on new { pid = gm.PersonId, gid = gm.GroupId } equals new { pid = user.Id, gid = pg.Id }
+                       join gs in context.GroupShares
+                           on pg.Id equals gs.GroupId
+                       where gs.FolderId == folder.Id
+                       select gm.PersonId).Any();
         }
 
         public static bool IsImageEditable(ApplicationUser user, Person owner, Image image, ScreenTakerEntities context)
