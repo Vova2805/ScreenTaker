6ff309b5226634fbe1930a84728435626dd22eff
Dudas <Dudas@VOVA-PC>
2016-06-07 20:17:10 +0200

    Bug fixing

--- ScreenTaker/Controllers/AccountController.cs
+++ ScreenTaker/Controllers/AccountController.cs
@@ -117,7 +117,8 @@ namespace ScreenTaker.Controllers
                 case SignInStatus.RequiresVerification:
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
-                //default:
+                default:
+                    { };
                 //    ModelState.AddModelError("", "Invalid login attempt.");
                     return View(model);
             }
