14a61cf4ca21c3ba525fa539e1ab324b78411add
Dudas <Dudas@VOVA-PC>
2016-06-07 21:54:09 +0200

    comment regex

--- ScreenTaker/Scripts/Site.js
+++ ScreenTaker/Scripts/Site.js
@@ -116,8 +116,8 @@ function copyToClipboard(elem) {
     return succeed;
 }
 function emailValidation(email) {
-    var pattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
-    if (email == '' || !pattern.test(email))
+    //var pattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
+    if (email == '')//|| !pattern.test(email)
     {
     return false;
     }
