3031109e00d40902538998ce4fe89df482ccb63f
Dudas <Dudas@VOVA-PC>
2016-06-07 22:17:40 +0200

    Bug fix

--- ScreenTaker/Scripts/Site.js
+++ ScreenTaker/Scripts/Site.js
@@ -12,15 +12,6 @@ function openNav() {
 
     document.getElementById("openSideMenu").style.disabled = true;
     document.getElementById("openSideMenu").style.backgroundColor = "#AAAAAA";
-    //$('#AddFolderButton').animate({
-    //    marginRight: "-40px"
-    //}, 500);
-    //$('#UploadImageButton').animate({
-    //    marginRight: "-40px"
-    //}, 500);
-    //$('#UploadImageSpan').animate({
-    //    marginRight: "-50px"
-    //}, 500);
 }
 
 
@@ -37,30 +28,7 @@ function closeNav() {
 
     document.getElementById("openSideMenu").style.disabled = false;
     document.getElementById("openSideMenu").style.backgroundColor = "white";
-    //$('#AddFolderButton').animate({
-    //    marginRight: "10px"
-    //}, 500);
-    //$('#UploadImageButton').animate({
-    //    marginRight: "10px"
-    //}, 500);
-    //$('#UploadImageSpan').animate({
-    //    marginRight: "-28px"
-    //}, 500);
 }
-
-$('#toggle_event_editing button').click(function () {
-    if ($(this).hasClass('locked_active') || $(this).hasClass('unlocked_inactive')) {
-        /* code to do when unlocking */
-        $('#switch_status').html('Switched on.');
-    } else {
-        /* code to do when locking */
-        $('#switch_status').html('Switched off.');
-    }
-
-    /* reverse locking status */
-    $('#toggle_event_editing button').eq(0).toggleClass('locked_inactive locked_active btn-default btn-info');
-    $('#toggle_event_editing button').eq(1).toggleClass('unlocked_inactive unlocked_active btn-info btn-default');
-});
 
 function selectText() {
     document.getElementById('sharedLink').select();
