a87504ae7db91d492e8857ff8977ad58c57b604d
alex bachkay <abachkay@gmail.com>
2016-06-14 20:01:42 +0300

    removed id from routeConfig

--- ScreenTaker/App_Start/RouteConfig.cs
+++ ScreenTaker/App_Start/RouteConfig.cs
@@ -16,8 +16,8 @@ namespace ScreenTaker
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Welcome", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Welcome" }
             );
 
             routes.MapRoute(
